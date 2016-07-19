1a0c91c3e0a02a4fb3b9cfa155980cc6d2b8d561
Ekta Arora <ekta.arora464@gmail.com>
2016-07-19 13:07:56 -0400

    overrode the createSourceVisitor method in checker class

--- IOEffect/src/CheckerDefaultPackage/IOEffectChecker.java
+++ IOEffect/src/CheckerDefaultPackage/IOEffectChecker.java
@@ -1,8 +1,39 @@
 package CheckerDefaultPackage;
+import org.checkerframework.common.basetype.BaseAnnotatedTypeFactory;
 import org.checkerframework.common.basetype.BaseTypeChecker;
+import org.checkerframework.common.basetype.BaseTypeVisitor;
 import org.checkerframework.framework.qual.StubFiles;
 import org.checkerframework.framework.source.SupportedLintOptions;
 
 @StubFiles({"org-eclipse.astub", "org-osgi.astub", "org-swtchart.astub"})
 @SupportedLintOptions({"debugSpew"})
-public class IOEffectChecker extends BaseTypeChecker {}
+public class IOEffectChecker extends BaseTypeChecker {
+	
+	@Override
+	protected BaseTypeVisitor<?> createSourceVisitor() {
+        /*// Try to reflectively load the visitor.
+        Class<?> checkerClass = this.getClass();
+
+        while (checkerClass != BaseTypeChecker.class) {
+            final String classToLoad =
+                    checkerClass
+                            .getName()
+                            .replace("Checker", "IOEffectVisitor")
+                            .replace("Subchecker", "IOEffectVisitor");
+            BaseTypeVisitor<?> result =
+                    invokeConstructorFor(
+                            classToLoad,
+                            new Class<?>[] {BaseTypeChecker.class},
+                            new Object[] {this});
+            if (result != null) {
+                return result;
+            }
+            checkerClass = checkerClass.getSuperclass();
+        }
+
+        // If a visitor couldn't be loaded reflectively, return the default.
+        return new BaseTypeVisitor<BaseAnnotatedTypeFactory>(this);*/
+		
+		return new IOEffectVisitor(this);
+    }
+}
